943b795b29300d85c8497bc8834f30dceec17b6a
unrealbootpower <46008343+Rossi-Davide@users.noreply.github.com>
2024-09-16 13:01:38 +0200

    http file retriever

--- http_server.py
+++ http_server.py
@@ -1,7 +1,7 @@
 #http server created from scratch
 #built on top of level 4 sever components built in python the 
 import socketserver
-import os
+import sys
 
 
 prova_obj = None
@@ -13,10 +13,10 @@ class RequestHandlerOne(socketserver.BaseRequestHandler):
 
         print(exception)
 
-
         ex_encoded = str(exception).encode('utf-8')
 
         self.request.sendall(ex_encoded)
+
 
 
     def handle(self):
@@ -44,31 +44,26 @@ class RequestHandlerOne(socketserver.BaseRequestHandler):
             
 
 
+            response = "HTTP/1.1 200 OK\r\nCache-Control: no-cache, private\r\nDate: Mon, 24 Nov 2014 10:21:21 GMT\r\n\r\n"
+            response_encoded = response.encode('utf-8')
+            
 
-
+            self.request.sendall(response_encoded)
 
         
             resource_name = type_request_descriptors[1].removeprefix('/')
 
 
+            with open(resource_name, 'rb') as file:
+
+                while buffer := file.read(4096):
+
+                    
+                    self.request.sendall(buffer)
 
 
-            file = open(resource_name, 'r')
+                
 
-            encoded_file = file.encode('utf-8')
-
-
-
-
-
-
-
-
-            response = "HTTP/1.1 200 OK\r\nCache-Control: no-cache, private\r\nContent-Length: 107\r\nDate: Mon, 24 Nov 2014 10:21:21 GMT\r\n\r\n<html><head><title></title></head><body>TIME : 1416824843 <br>DATE: Mon Nov 24 15:57:23 2014 </body></html>"
-
-            response_encoded = response.encode('utf-8')
-
-            self.request.sendall(response_encoded)
 
 
         except (Exception,OSError) as e:
